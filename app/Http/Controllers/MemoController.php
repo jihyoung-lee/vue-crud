5c74b1ff1d131312603d61e972bf3e089b05ff0e
지형 <jihyoung.lee90@gmail.com>
2022-09-28 20:03:21 +0900

    Update MemoController.php
    store

--- app/Http/Controllers/MemoController.php
+++ app/Http/Controllers/MemoController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\MemoList;
 use Illuminate\Http\Request;
 
 class MemoController extends Controller
 {
-    
+    public function store()
+    {
+        $validated = request()->validate(
+            ['title'=> 'required',
+            ] );
+
+        $memoList = MemoList::create($validated);
+
+        return response()->json([
+            'memoList' => $memoList,
+        ], 200);
+    }
 }
