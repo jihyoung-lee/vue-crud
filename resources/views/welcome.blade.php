5b461ed113bdc750112abe4390e3b420a22d4284
지형 <jihyoung.lee90@gmail.com>
2022-09-28 20:04:00 +0900

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="csrf-token" value="{{ csrf_token() }}"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
@@ -13,4 +14,5 @@
     <memo-component></memo-component>
 </div>
 </body>
+
 </html>
