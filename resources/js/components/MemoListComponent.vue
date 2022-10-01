0254f4543d9cbe7dcf818fb726185b22b9250112
지형 <jihyoung.lee90@gmail.com>
2022-10-02 08:44:00 +0900

    Create MemoListComponent.vue
    메모리스트 받아오는 컴포넌트

---
+++ resources/js/components/MemoListComponent.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="memo-side" >
+    <div class="top-wrapper w-128 flex justify-between pb-8 px-6 py-8  bg-amber-300 hover:bg-amber-500 rounded-t-lg " v-for="(item,idx) in memoList" :key="idx"  >
+
+            <div class="new-title text-3xl font-bold ">
+                <div pt-3>
+                    {{item.title}}
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        memo: {
+            type:Object,
+        },
+    },
+    data() {
+        return {
+            memoList: []
+        }
+    },
+    created() {
+        axios.get('api/memo').then(res => {
+            this.memoList = res.data.memoList;
+            console.log(res.data.memoList)
+        }).catch(error => {
+            console.log(error)
+        });}
+
+}
+</script>
