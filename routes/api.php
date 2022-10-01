bfc9fd0269f288eaae2bba90d3f927c5c963be4a
지형 <jihyoung.lee90@gmail.com>
2022-10-02 08:44:18 +0900

    Update api.php
    api 라우터추가

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::post('/memo/title',[Memocontroller::class, 'store']);
+Route::get('/memo/',[MemoController::class,'index']);
