4dd59cdaf821ee6a717165f281eb92295754863d
Jihyoung-Lee <jihyoung.lee90@gmail.com>
2022-10-26 11:44:58 +0900

    수정

--- resources/js/components/MemoComponent.vue
+++ resources/js/components/MemoComponent.vue
@@ -2,6 +2,9 @@
     <div class="container">
         <div id="menu_wrap" class="bg-yellow-600 text-black w-128 px-1 py-2">
             <div class="option">
+                <button class="py-2 px-4 rounded-lg shadow-md text-white bg-blue-500" v-on:click="getData">
+                    Click me [{{title}}]
+                </button>
                 <button class="absolute mr-2">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width={1.5} stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M8.625 9.75a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 01.778-.332 48.294 48.294 0 005.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
@@ -17,10 +20,7 @@
             </div>
 
             <memo-list-component></memo-list-component>
-            <button class="py-2 px-4 rounded-lg shadow-md text-white bg-blue-500" v-on:click="getData">
-                Click me [{{title}}]
 
-            </button>
         </div>
 
     </div>
