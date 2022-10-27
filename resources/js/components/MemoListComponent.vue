ae5629d524f6b2e92038508dcb162cbb99c82064
Jihyoung-Lee <jihyoung.lee90@gmail.com>
2022-10-27 16:46:57 +0900

    ㄹ

--- resources/js/components/MemoListComponent.vue
+++ resources/js/components/MemoListComponent.vue
@@ -1,14 +1,5 @@
 <template>
-    <div class="memo-side" >
-    <div class="top-wrapper w-128 flex justify-between pb-8 px-6 py-8  bg-amber-300 hover:bg-amber-500 rounded-t-lg " v-for="(item,idx) in memoList" :key="idx"  >
 
-            <div class="new-title text-3xl font-bold ">
-                <div pt-3>
-                    {{item.title}}
-                </div>
-            </div>
-        </div>
-    </div>
 </template>
 <script>
 export default {
--- webpack.mix.js
+++ webpack.mix.js
@@ -16,4 +16,5 @@ mix.js('resources/js/app.js', 'public/js')
         require("tailwindcss"),
     ])
     .vue()
+    .version()
     //.sass('resources/sass/app.scss', 'public/css');
